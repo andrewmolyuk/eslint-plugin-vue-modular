f50267835a5e99087ae20986b6d468016fd77b16
Andrew Molyuk <andrew@molyuk.com>
2025-09-14 22:07:48 +0300

    fix: enhance merge commit filtering in release notes configuration

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -17,6 +17,8 @@ module.exports = {
             if (commit.header && typeof commit.header === 'string') {
               if (commit.header.startsWith('chore(pr):')) return null
               if (commit.header.startsWith('chore(merge):')) return null
+              if (commit.header.startsWith('Merge pull request')) return null
+              if (commit.header.startsWith('Merge branch')) return null
             }
 
             // Return a shallow copy instead of mutating the provided object.
