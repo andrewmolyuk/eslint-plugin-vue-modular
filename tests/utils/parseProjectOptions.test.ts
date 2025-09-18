7676cda8640b2996b22cf33a7c8d30040ec8c838
Andrew Molyuk <andrew@cgstowernetworks.com>
2025-09-18 12:18:51 +0300

    fix: add type annotations for VueModularRuleContext in parseProjectOptions tests

--- tests/utils/parseProjectOptions.test.ts
+++ tests/utils/parseProjectOptions.test.ts
@@ -1,13 +1,14 @@
 import { describe, it, expect, beforeEach } from 'vitest'
 import { parseProjectOptions } from '../../src/utils/parseProjectOptions'
 import { defaultProjectOptions } from '../../src/projectOptions'
+import type { VueModularRuleContext } from '../../src/types'
 import { setupTest } from '../test-utils'
 
 describe('parseProjectOptions', () => {
   beforeEach(() => setupTest())
 
   it('returns the first option when options array is present', () => {
-    const context = { settings: { 'vue-modular': { rootPath: 'my-app', rootAlias: 'alias' } } }
+    const context = { settings: { 'vue-modular': { rootPath: 'my-app', rootAlias: 'alias' } } } as VueModularRuleContext
     const expectedOptions = structuredClone(defaultProjectOptions)
     expectedOptions.rootPath = 'my-app'
     expectedOptions.rootAlias = 'alias'
@@ -15,22 +16,22 @@ describe('parseProjectOptions', () => {
     expect(parseProjectOptions(context)).toEqual(expectedOptions)
   })
   it('returns default object when options array is empty', () => {
-    const context = { options: [] }
+    const context: VueModularRuleContext = { options: [] } as unknown as VueModularRuleContext
     expect(parseProjectOptions(context)).toEqual(defaultProjectOptions)
   })
 
   it('returns default object when options is undefined', () => {
-    const context = {}
+    const context: VueModularRuleContext = {} as VueModularRuleContext
     expect(parseProjectOptions(context)).toEqual(defaultProjectOptions)
   })
 
   it('returns default object when options[0] is falsy', () => {
-    const context = { options: [null] }
+    const context: VueModularRuleContext = { options: [null] } as VueModularRuleContext
     expect(parseProjectOptions(context)).toEqual(defaultProjectOptions)
   })
 
   it('returns default object when options[0] is undefined', () => {
-    const context = { options: [undefined] }
+    const context: VueModularRuleContext = { options: [undefined] } as VueModularRuleContext
     expect(parseProjectOptions(context)).toEqual(defaultProjectOptions)
   })
 })
