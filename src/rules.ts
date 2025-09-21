34dce432b1f45c2966fa33f29b8d30907a632395
Andrew Molyuk <andrew@molyuk.com>
2025-09-21 13:30:16 +0300

    refactor: reorganize imports and update rules structure for clarity

--- src/rules.ts
+++ src/rules.ts
@@ -1,32 +1,36 @@
+import { appImports } from './rules/app-imports'
 import { componentsIndexRequired } from './rules/components-index-required'
-import { featureImports } from './rules/feature-imports'
 import { crossImportsAbsolute } from './rules/cross-imports-alias'
+import { featureImports } from './rules/feature-imports'
 import { featureIndexRequired } from './rules/feature-index-required'
 import { fileComponentNaming } from './rules/file-component-naming'
 import { fileTsNaming } from './rules/file-ts-naming'
 import { folderKebabCase } from './rules/folder-kebab-case'
+import { internalImportsRelative } from './rules/internal-imports-relative'
+import { serviceFilenameNoSuffix } from './rules/service-filename-no-suffix'
+import { sfcOrder } from './rules/sfc-order'
 import { sfcRequired } from './rules/sfc-required'
 import { sharedImports } from './rules/shared-imports'
-import { appImports } from './rules/app-imports'
-import { serviceFilenameNoSuffix } from './rules/service-filename-no-suffix'
-import { storeFilenameNoSuffix } from './rules/store-filename-no-suffix'
 import { sharedUiIndexRequired } from './rules/shared-ui-index-required'
+import { storeFilenameNoSuffix } from './rules/store-filename-no-suffix'
 import { viewsSuffix } from './rules/views-suffix'
 import { VueModularRuleModule } from './types'
 
 export const rules: Record<string, VueModularRuleModule> = {
+  'app-imports': appImports,
   'components-index-required': componentsIndexRequired,
+  'cross-imports-alias': crossImportsAbsolute,
+  'feature-imports': featureImports,
   'feature-index-required': featureIndexRequired,
   'file-component-naming': fileComponentNaming,
   'file-ts-naming': fileTsNaming,
   'folder-kebab-case': folderKebabCase,
-  'shared-ui-index-required': sharedUiIndexRequired,
+  'internal-imports-relative': internalImportsRelative,
+  'service-filename-no-suffix': serviceFilenameNoSuffix,
+  'sfc-order': sfcOrder,
   'sfc-required': sfcRequired,
   'shared-imports': sharedImports,
-  'app-imports': appImports,
-  'service-filename-no-suffix': serviceFilenameNoSuffix,
+  'shared-ui-index-required': sharedUiIndexRequired,
   'store-filename-no-suffix': storeFilenameNoSuffix,
-  'cross-imports-alias': crossImportsAbsolute,
-  'feature-imports': featureImports,
   'views-suffix': viewsSuffix,
 }
